3396432dae35150fede3b0a95b9c3dae7b7a8754
baitiankanbujian888 <45777149+baitiankanbujian888@users.noreply.github.com>
2018-12-11 11:30:41 +0800

    Add files via upload

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@
  */
 //% weight=100 color=#000011 icon="\uf185"
 namespace COMMU {
-  
+
     //% blockId="COMMU_INIT" block="INIT"
     //% weight=100 blockGap=8
 
@@ -13,18 +13,19 @@ namespace COMMU {
         serial.redirect(SerialPin.P0, SerialPin.P1, 115200)
     }
 
- 
-   
+
+
     //% blockId="COMMU_EXIT" block="EIXT"
     //% weight=90 blockGap=8
-	export function exit(): void {
+    //% blockGap=50
+    export function exit(): void {
         serial.redirectToUSB()
     }
 
 
     //% blockId="COMMU_TRANSFER" block="TRANSFER"
     //% weight=80 blockGap=8
-	export function transfer(): void {
+    export function transfer(): void {
         serial.writeString("motor")
     }
 
